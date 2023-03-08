e314750c661402b19a7ca71ee15e3c47d53c91a4
dbgys1127 <dbgys1127@gmail.com>
2023-03-08 15:14:55 +0900

    fix : h2, redis 설정 변경

--- movie/src/main/java/shallwe/movie/config/RedisConfig.java
+++ movie/src/main/java/shallwe/movie/config/RedisConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import redis.embedded.RedisServer;
+
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -19,22 +19,22 @@ public class RedisConfig {
     @Value("${spring.redis.port}")
     private int redisPort;
 
-    private RedisServer redisServer;
+//    private RedisServer redisServer;
 
-    //로컬에서 레디스 서버 자동 시작
-    @PostConstruct
-    public void redisStart() throws IOException {
-        redisServer = new RedisServer(redisPort);
-        redisServer.start();
-    }
-
-    //로컬에서 레디스 서버 자동 정지
-    @PreDestroy
-    public void stopRedis() {
-        if (redisServer != null) {
-            redisServer.stop();
-        }
-    }
+//    //로컬에서 레디스 서버 자동 시작
+//    @PostConstruct
+//    public void redisStart() throws IOException {
+//        redisServer = new RedisServer(redisPort);
+//        redisServer.start();
+//    }
+//
+//    //로컬에서 레디스 서버 자동 정지
+//    @PreDestroy
+//    public void stopRedis() {
+//        if (redisServer != null) {
+//            redisServer.stop();
+//        }
+//    }
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
