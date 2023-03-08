7bf479168403348c451a2fe36b774161116ae1d4
dbgys1127 <dbgys1127@gmail.com>
2023-03-08 16:00:39 +0900

    feat: nginx 로드밸런싱 구현

--- movie/src/main/java/shallwe/movie/security/controller/TestController.java
+++ movie/src/main/java/shallwe/movie/security/controller/TestController.java
@@ -1,15 +1,21 @@
 package shallwe.movie.security.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @Controller
 public class TestController {
+    @Value("${server.port}")
+    private String port;
     @GetMapping("/")
     public String welcomePage() {
+        log.info("now port {}",port);
         return "index";
     }
 }
